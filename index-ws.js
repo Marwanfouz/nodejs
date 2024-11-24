81a116b70a4e3fb90aa2ce088ecbd950e2c2dcab
Marwan Fouz <marwanfouz76@gmail.com>
2024-11-24 20:48:15 +0200

    add database logging

--- index-ws.js
+++ index-ws.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const server = require('http').createServer();
 const app = express();
-const PORT = 3000;
 
 app.get('/', function(req, res) {
   res.sendFile('index.html', {root: __dirname});
@@ -9,7 +8,17 @@ app.get('/', function(req, res) {
 
 server.on('request', app);
 
-server.listen(PORT, function () { console.log('Listening on ' + PORT); });
+process.on('SIGINT', () => {
+  console.log('sigint');
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(() => {
+    shutdownDB(); 
+  });
+});
+
+server.listen(3000, function () { console.log('Listening on 3000'); });
 
 /** Websocket **/
 const WebSocketServer = require('ws').Server;
@@ -20,6 +29,9 @@ wss.on('connection', function connection(ws) {
   const numClients = wss.clients.size;
 
   console.log('clients connected: ', numClients);
+
+  db.run(`INSERT INTO visitors (count, time)
+    VALUES (${numClients}, datetime('now'))`);
 
   wss.broadcast(`Current visitors: ${numClients}`);
 
@@ -33,7 +45,7 @@ wss.on('connection', function connection(ws) {
   });
 
   ws.on('error', function error() {
-    //
+
   });
 });
 
@@ -44,3 +56,32 @@ wss.broadcast = function broadcast(data) {
   });
 };
 /** End Websocket **/
+
+/** Database stuff **/
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(':memory:');
+
+// .seralize ensures DB is set up before any queries
+db.serialize(() => {
+    db.run(`CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+      )`);
+});
+
+function getCounts() {
+    db.each("SELECT * FROM visitors", (err, row) => {
+      console.log(row);
+    });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log('shutting down DB');
+  db.close();
+}
+
+
+
+/** End Database stuff **/
